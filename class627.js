df8ae094de87535e86b6ab88373dbb5f91a59549
Sandra <Sandra@Sandras-MacBook-Air.local>
2017-07-12 19:02:35 -0400

    liri

---
+++ class627.js
@@ -0,0 +1,17 @@
+
+
+var dogs = {
+0: true,
+1: "Woof!",
+2: function(){
+	console.log(Object.keys(dogs))
+};
+}
+
+var cats= [
+raining= false,
+noise= "Meow!",
+makeNoise= function(){
+	console.log(Object.keys(cats))
+};
+]
